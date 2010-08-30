833fd80350fa3c9c331e2fbb4c44ffdb5096398f
Jonas Folleso <jonas@follesoe.no>
2010-08-30 18:36:37 +0200

    Moved the search code into the view model to keep things simple for the demo.

--- TwitterWall/MainPage.xaml.cs
+++ TwitterWall/MainPage.xaml.cs
@@ -1,20 +1,10 @@
-﻿using System.Collections.Generic;
-
-namespace TwitterWall
+﻿namespace TwitterWall
 {
     public partial class MainPage
     {
         public MainPage()
         {
             InitializeComponent();
-
-            var twitterSearch = new TwitterSearch();
-            twitterSearch.Search("frontend2010", Done);
-        }
-
-        private void Done(List<Tweet> tweets)
-        {
-            
         }
     }
 }
--- TwitterWall/SearchViewModel.cs
+++ TwitterWall/SearchViewModel.cs
@@ -1,17 +1,23 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Net;
+using System.Linq;
 using System.Collections.Generic;
+using System.Windows.Media.Imaging;
 using System.Collections.ObjectModel;
+using System.ServiceModel.Syndication;
 
 using GalaSoft.MvvmLight;
-using GalaSoft.MvvmLight.Command;
 
 namespace TwitterWall
 {
     public class SearchViewModel : ViewModelBase
-    {        
+    {
+        private const string ApiUrl = "http://search.twitter.com/search.atom?q={0}&since_id={1}";
+
         public ObservableCollection<Tweet> Tweets { get; private set; }
-        public RelayCommand SearchCommand { get; private set; }
-        private readonly TwitterSearch _search;
+        private string _sinceId;
         private string _keyword;
         
         public string Keyword
@@ -27,32 +33,44 @@ namespace TwitterWall
         
         public SearchViewModel()
         {
-            _search = new TwitterSearch();
-            Tweets = new ObservableCollection<Tweet>();
-            SearchCommand = new RelayCommand(Search);            
-        }
-
-        public void Search()
-        {
-            if(!string.IsNullOrEmpty(Keyword))
-            {
-                _search.Search(Keyword, AddTweets);
-            }
-        }
-
-        private int counter = 0;
-
-        public void AddRandom()
-        {
-            counter++;
-            var t = new Tweet();
-            t.Text = "Hello World! " + counter;
-            Tweets.Insert(0, t);
+            Tweets = new ObservableCollection<Tweet>();      
         }
 
         private void AddTweets(List<Tweet> newTweets)
         {
-            Deployment.Current.Dispatcher.BeginInvoke(() => newTweets.ForEach(t => Tweets.Insert(0, t)));
+            newTweets.ForEach(t => Tweets.Insert(0, t));
+        }
+
+        public void Search()
+        {
+            var url = new Uri(string.Format(ApiUrl, _keyword, _sinceId));
+
+            var client = new WebClient();
+            client.DownloadStringCompleted += (o, e) => AddTweets(ParseXml(e.Result));
+            client.DownloadStringAsync(url);
+        }
+
+        private List<Tweet> ParseXml(string xmlString)
+        {
+            var reader = XmlReader.Create(new StringReader(xmlString));
+
+            var feed = SyndicationFeed.Load(reader);
+            var tweetQuery = from item in feed.Items
+                             select new Tweet
+                             {
+                                 Text = item.Title.Text,
+                                 CreatedAt = item.PublishDate.DateTime,
+                                 FromUser = item.Authors.First().Name,
+                                 ProfileImageUrl = item.Links.Where(l => l.RelationshipType == "image").Single().Uri,
+                                 ProfileImage = new BitmapImage(item.Links.Where(l => l.RelationshipType == "image").Single().Uri),
+                                 Id = item.Id.Split(',')[1].Split(':')[1]
+                             };
+
+            var tweets = tweetQuery.ToList();
+            if (tweets.Count > 0)
+                _sinceId = tweets.First().Id;
+
+            return tweets;
         }
     }
 }
